16f023b3e8f2a96b4fe7134d7b2a51f7c7cab798
Gamer_Z <grasmanek94@gmail.com>
2016-06-21 02:00:28 +0200

    initial commit

---
+++ music/music.ino
@@ -0,0 +1,124 @@
+#pragma GCC optimize ("-O2") 
+
+long g(long i, long x, long t, long o)
+{
+    return ((3 & x&(i*((3 & i >> 16 ? "BY}6YB6%":"Qj}6jQ6%")[t%8]+51)>>o))<<4);
+};
+
+volatile long i = 120000; // skip a bit from the beginning
+volatile long n = 0x9591ba8;
+volatile long s = 0x9591bb8;
+
+uint8_t getTone()
+{
+    i += 2 + (i % 2); // play song a bit faster because arduino uno can't do these calculations fast enough
+    return (g(i, 1, n = i >> 14, 12) + g(i, s = i >> 17, n^i >> 13, 10) + g(i, s / 3, n + ((i >> 11) % 3), 10) + g(i, s / 5, 8 + n - ((i >> 10) % 3), 9));
+}
+
+#include <stdint.h>
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+
+#define SAMPLE_RATE 8000
+
+int speakerPin = 11; // Can be either 3 or 11, two PWM outputs connected to Timer 2
+
+void stopPlayback()
+{
+    // Disable playback per-sample interrupt.
+    TIMSK1 &= ~_BV(OCIE1A);
+
+    // Disable the per-sample timer completely.
+    TCCR1B &= ~_BV(CS10);
+
+    // Disable the PWM timer.
+    TCCR2B &= ~_BV(CS10);
+
+    digitalWrite(speakerPin, LOW);
+}
+
+// This is called at 8000 Hz to load the next sample.
+ISR(TIMER1_COMPA_vect) 
+{
+    //if(speakerPin==11)
+    //{
+        OCR2A = getTone();
+    //} else {
+    //    OCR2B = getTone();            
+    //}
+}
+
+void startPlayback()
+{
+    pinMode(speakerPin, OUTPUT);
+
+    // Set up Timer 2 to do pulse width modulation on the speaker
+    // pin.
+
+    // Use internal clock (datasheet p.160)
+    ASSR &= ~(_BV(EXCLK) | _BV(AS2));
+
+    // Set fast PWM mode  (p.157)
+    TCCR2A |= _BV(WGM21) | _BV(WGM20);
+    TCCR2B &= ~_BV(WGM22);
+
+    if(speakerPin==11){
+        // Do non-inverting PWM on pin OC2A (p.155)
+        // On the Arduino this is pin 11.
+        TCCR2A = (TCCR2A | _BV(COM2A1)) & ~_BV(COM2A0);
+        TCCR2A &= ~(_BV(COM2B1) | _BV(COM2B0));
+        // No prescaler (p.158)
+        TCCR2B = (TCCR2B & ~(_BV(CS12) | _BV(CS11))) | _BV(CS10);
+
+        // Set initial pulse width to the first sample.
+        OCR2A = getTone();
+    } else {
+        // Do non-inverting PWM on pin OC2B (p.155)
+        // On the Arduino this is pin 3.
+        TCCR2A = (TCCR2A | _BV(COM2B1)) & ~_BV(COM2B0);
+        TCCR2A &= ~(_BV(COM2A1) | _BV(COM2A0));
+        // No prescaler (p.158)
+        TCCR2B = (TCCR2B & ~(_BV(CS12) | _BV(CS11))) | _BV(CS10);
+
+        // Set initial pulse width to the first sample.
+        OCR2B = getTone();
+    }
+
+
+
+
+
+    // Set up Timer 1 to send a sample every interrupt.
+
+    cli();
+
+    // Set CTC mode (Clear Timer on Compare Match) (p.133)
+    // Have to set OCR1A *after*, otherwise it gets reset to 0!
+    TCCR1B = (TCCR1B & ~_BV(WGM13)) | _BV(WGM12);
+    TCCR1A = TCCR1A & ~(_BV(WGM11) | _BV(WGM10));
+
+    // No prescaler (p.134)
+    TCCR1B = (TCCR1B & ~(_BV(CS12) | _BV(CS11))) | _BV(CS10);
+
+    // Set the compare register (OCR1A).
+    // OCR1A is a 16-bit register, so we have to do this with
+    // interrupts disabled to be safe.
+    OCR1A = F_CPU / SAMPLE_RATE;    // 16e6 / 8000 = 2000
+
+    // Enable interrupt when TCNT1 == OCR1A (p.136)
+    TIMSK1 |= _BV(OCIE1A);
+
+    sei();
+}
+
+
+void setup()
+{
+    startPlayback();
+}
+
+void loop()
+{
+    while (true);
+}
